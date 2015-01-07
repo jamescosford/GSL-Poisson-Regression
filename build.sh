60b8034e0b6d22a4b5821eee34438c72778f4257
Modi <jamescosford@gmail.com>
2015-01-07 11:39:27 +0800

    Tidies up and includes code

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+gcc -c -Wall -Werror -fpic -std=c99 gsl_poisson_reg.c
+#gcc -shared -o libgsl_poisson_reg.so gsl_poisson_reg.o
+ar rcs libgsl_poisson_reg.a gsl_poisson_reg.o
+gcc -o mm libtest.c -I/usr/include/gsl -lgsl_poisson_reg -L`pwd` -std=c99 -lgsl -lgslcblas -lm
+cp libgsl_poisson_reg.so /usr/local/lib
+ldconfig
